d7bf6a3ac9643325a443fc15b0e75ac57159e424
Victor Parra <pvictor3@gmail.com>
2019-02-15 12:13:20 -0600

    First commit from Android Studio

--- app/src/main/java/com/contenidofoo1/pruebable/MainActivity.java
+++ app/src/main/java/com/contenidofoo1/pruebable/MainActivity.java
@@ -21,13 +21,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+
 
 
 public class MainActivity extends AppCompatActivity {
